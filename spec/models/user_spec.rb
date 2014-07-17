419746d73310afc1a829b317b70affd1cf51459c
Talya Stern <talya.stern@klarna.com>
2014-07-17 18:32:53 +0300

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  
+
 
   before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")}
 
@@ -16,6 +16,11 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
 
   it {should be_valid}
@@ -48,8 +53,8 @@ describe User do
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com, foo@bar..com]
-      addresses.each do |invalid_address|
+       foo@bar_baz.com foo@bar+baz.com, foo@bar..com]
+       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
       end
@@ -66,7 +71,7 @@ describe User do
     end
   end
 
-   describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -76,65 +81,109 @@ describe User do
     it { should_not be_valid }
   end
 
-	describe "when password is not present" do
-	  before do
-	    @user = User.new(name: "Example User", email: "user@example.com",
-	                     password: " ", password_confirmation: " ")
-	  end
-	  it { should_not be_valid }
-	end
+  describe "when password is not present" do
+   before do
+     @user = User.new(name: "Example User", email: "user@example.com",
+      password: " ", password_confirmation: " ")
+   end
+   it { should_not be_valid }
+ end
 
-	describe "when password doesn't match confirmation" do
-	  before { @user.password_confirmation = "mismatch" }
-	  it { should_not be_valid }
-	end
+ describe "when password doesn't match confirmation" do
+   before { @user.password_confirmation = "mismatch" }
+   it { should_not be_valid }
+ end
 
-  describe "email address with mixed case" do
-    let(:mixed_case_email){ "Foo@Example.CoM" }
+ describe "email address with mixed case" do
+  let(:mixed_case_email){ "Foo@Example.CoM" }
 
-    it "should be saved at all lower-case" do
-      @user.email = mixed_case_email
-      @user.save
-      expect(@user.reload.email).to eq mixed_case_email.downcase
-    end
+  it "should be saved at all lower-case" do
+    @user.email = mixed_case_email
+    @user.save
+    expect(@user.reload.email).to eq mixed_case_email.downcase
   end
-  
-  describe "remember_token" do
-    before { @user.save }
-    its(:remember_token) {should_not be_blank}
+end
+
+describe "remember_token" do
+  before { @user.save }
+  its(:remember_token) {should_not be_blank}
+end
+
+describe "micropost associations" do
+
+  before { @user.save }
+  let!(:older_micropost) do
+    FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
   end
-  
-  describe "micropost associations" do
+  let!(:newer_micropost) do
+    FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+  end
 
-    before { @user.save }
-    let!(:older_micropost) do
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
-    end
-    let!(:newer_micropost) do
-      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
-    end
-
-    it "should have the right microposts in the right order" do
-      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
-    end
-
-    it "should destroy associated microposts" do
-      microposts = @user.microposts.to_a
-      @user.destroy
-      expect(microposts).not_to be_empty
-      microposts.each do |micropost|
-        expect(Micropost.where(id: micropost.id)).to be_empty
-      end
-    end
-
-    describe "status" do
+  describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+    
+  it "should have the right microposts in the right order" do
+    expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+  end
+
+  it "should destroy associated microposts" do
+    microposts = @user.microposts.to_a
+    @user.destroy
+    expect(microposts).not_to be_empty
+    microposts.each do |micropost|
+      expect(Micropost.where(id: micropost.id)).to be_empty
     end
   end
+
+  describe "status" do
+    let(:unfollowed_post) do
+      FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+    end
+
+    its(:feed) { should include(newer_micropost) }
+    its(:feed) { should include(older_micropost) }
+    its(:feed) { should_not include(unfollowed_post) }
+  end
+end
+
+describe "following" do
+  let(:other_user) { FactoryGirl.create(:user) }
+  before do
+    @user.save
+    @user.follow!(other_user)
+  end
+
+  it { should be_following(other_user) }
+  its(:followed_users) { should include(other_user) }
+
+  describe "followed user" do
+    subject { other_user }
+    its(:followers) { should include(@user) }
+  end
+
+  describe "and unfollowing" do
+    before { @user.unfollow!(other_user) }
+
+    it { should_not be_following(other_user) }
+    its(:followed_users) { should_not include(other_user) }
+  end
+end
 end
